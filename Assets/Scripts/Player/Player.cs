15771b61a8d6ee95b6fc930bc531b323286d9700
Glebfra <gleb.frantsuzov@yandex.ru>
2024-06-30 23:53:13 +0300

    ! Init Commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    [RequireComponent(typeof(PlayerController), typeof(Rigidbody))]
+    public class Player : MonoBehaviour
+    {
+        private void Start()
+        {
+            gameObject.GetComponent<PlayerController>();
+            gameObject.GetComponent<Rigidbody>();
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private const float Speed = 1.0f;
+        private const float RotationSpeed = 5.0f;
+        private const float CameraSensitive = 500f;
+
+        private float _yaw;
+        private float _pitch;
+
+        private GameObject _springArm;
+        private GameObject _camera;
+
+        private Transform _cameraTransform;
+        
+        private void Start()
+        {
+            _springArm = new GameObject("Spring Arm")
+            {
+                transform =
+                {
+                    parent = transform,
+                    localPosition = new Vector3()
+                }
+            };
+
+            _camera = new GameObject("Camera")
+            {
+                transform =
+                {
+                    parent = _springArm.transform,
+                    localPosition = new Vector3()
+                }
+            };
+            _camera.transform.localPosition -= transform.forward * 2;
+            _camera.AddComponent<Camera>();
+            _cameraTransform = _camera.transform;
+        }
+
+        private void Update()
+        {
+            _yaw += Input.GetAxis("Mouse X") * CameraSensitive * Time.deltaTime;
+            _pitch -= Input.GetAxis("Mouse Y") * CameraSensitive * Time.deltaTime;
+            _pitch = Mathf.Clamp(_pitch, -90.0f, 90.0f);
+            _springArm.transform.rotation = Quaternion.Euler(_pitch, _yaw, 0.0f);
+
+            if (Input.GetAxis("Vertical") == 0 && Input.GetAxis("Horizontal") == 0) return;
+            Vector3 dir =
+                (transform.forward * Input.GetAxis("Vertical") +
+                 transform.right * Input.GetAxis("Horizontal")) * (Time.deltaTime * Speed);
+            dir.y = 0;
+            transform.Translate(dir);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, Quaternion.Euler(0, _yaw, 0), Time.deltaTime * RotationSpeed);
+        }
+    }
+}
